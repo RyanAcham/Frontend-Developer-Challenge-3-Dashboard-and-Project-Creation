9dbf0bc346f020ed7f8cfdaeef5db6eae487e5f0
Ryan Acham <ryanacham@hotmail.com>
2023-11-19 11:09:22 -0500

    added header and footer; bug fixing

--- assets/App.js
+++ assets/App.js
@@ -1,10 +1,10 @@
 import "bootstrap/dist/css/bootstrap.css"
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import header from "./assets/header";
-import mainpage from "./assets/mainpage";
-import newproject from "./assets/newproject";
-import footer from "./assets/footer";
+import Header from "./assets/header";
+import Dashboard from "./assets/mainpage";
+import NewProject from "./assets/newproject";
+import Footer from "./assets/footer";
 
 
 function App() {
@@ -14,7 +14,7 @@ function App() {
           <Header />
           <Routes>
             <Route exact path="/" element={<Dashboard />} />
-            <Route exact path="/newproject" element={<newproject />} />
+            <Route exact path="/newproject" element={<NewProject />} />
           </Routes>
           <Footer />
     </Router>
